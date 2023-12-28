72c9e75012c655ba4a2fffbb6afe1e50ee7471cc
sanidhya1912c <sanidhyac1912@gmail.com>
2023-12-28 23:28:40 +0530

    learn a lot about the middleware, routing and the pathing of files, how you can serve the files etc

--- Backend-In-Js/Module-2/app.js
+++ Backend-In-Js/Module-2/app.js
@@ -1,1 +1,28 @@
-console.log('hello world ')
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const adminRouter = require("./routes/add-product");
+
+const userRouter = require("./routes/shop");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.use("/admin", adminRouter);
+app.use(userRouter);
+
+app.use((req, res, next) =>
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
+);
+
+// app.use("/", (req, res, next) => {
+//   console.log("This always run ");
+//   next();
+// });
+
+
+app.listen(3000);
