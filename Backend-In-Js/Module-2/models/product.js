69bcc8822cfb44fea9393d85de34e76a70e8b843
sanidhya1912c <sanidhyac1912@gmail.com>
2024-01-04 22:22:08 +0530

    .

--- Backend-In-Js/Module-2/models/product.js
+++ Backend-In-Js/Module-2/models/product.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -18,21 +18,18 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, price, description) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
-    this.price = price;
+    this.imageUrl = imageUrl;
     this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile((products) => {
-      products.push({
-        title: this.title,
-        price: this.price,
-        description: this.description,
-      });
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        // console.log(err);
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
       });
     });
   }
--- Backend-In-Js/Module-2/routes/shop.js
+++ Backend-In-Js/Module-2/routes/shop.js
@@ -6,12 +6,14 @@ const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/' , shopController.getShop)
+router.get('/', shopController.getIndex);
 
-router.get('/product', shopController.getProductsList);
+router.get('/products', shopController.getProducts);
 
-router.get('/cart' , shopController.getCart)
+router.get('/cart', shopController.getCart);
 
-router.get('/checkout' , shopController.getCheckout)
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
