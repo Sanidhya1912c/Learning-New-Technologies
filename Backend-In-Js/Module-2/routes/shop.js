2b9929216b52e4a2d2065bc251792ae18b001706
sanidhya1912c <sanidhyac1912@gmail.com>
2024-01-05 21:44:02 +0530

    add the functionality for the add to cart button

--- Backend-In-Js/Module-2/routes/shop.js
+++ Backend-In-Js/Module-2/routes/shop.js
@@ -11,10 +11,10 @@ router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProductsId)
-// display: flex;
-// justify-content: center;
 
 router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart)
 
 router.get('/orders', shopController.getOrders);
 
