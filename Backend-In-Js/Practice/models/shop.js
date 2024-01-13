1414a50cbabf6b888a926f6065dab08276dd6741
sanidhya1912c <sanidhyac1912@gmail.com>
2024-01-13 15:39:51 +0530

    .

--- Backend-In-Js/Practice/models/shop.js
+++ Backend-In-Js/Practice/models/shop.js
@@ -2,6 +2,6 @@ module.exports = class Product {
   constructor(titel, price, description, imageUrl) {
     this.titel = titel
     this.price = price
-    this.description = 
+    this.description = description
   }
 };
