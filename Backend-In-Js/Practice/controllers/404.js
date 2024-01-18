018ed98285db809acd9eae98e760b7d091006390
Sanidhya1912 <sanidhyac1912@gmail.com>
2024-01-18 21:17:25 +0530

    deleated the practice, no need to do practice i guss

--- Backend-In-Js/Practice/controllers/404.js
+++
@@ -1,3 +0,0 @@
-module.exports = getPageNotFound = (req, res, next) => {
-  res.render("404.ejs");
-};
--- Backend-In-Js/Practice/models/shop.js
+++
@@ -1,8 +0,0 @@
-module.exports = class Product {
-  constructor(titel, price, description, imageUrl) {
-    this.titel = titel
-    this.price = price
-    this.description = description
-    this.imageUrl = imageUrl
-  }
-};
